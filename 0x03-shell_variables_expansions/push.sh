b3ed34e8fcdb5e3811e0db42c2a8ee8ab601f452
bossclement <bosstclement@gmail.com>
2023-02-14 12:17:47 -0800

    New commit

---
+++ 0x03-shell_variables_expansions/push.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+comment="New commit"
+
+# check if task file exists
+if [ -f "$1" ];
+then
+	# change the file to executable.
+	chmod u+x $1
+
+	# check if the comment was provided and use it.
+	if [ "$2" != "" ];
+	then
+		comment=$2
+	fi
+
+	# push the file to git
+	git add .
+	git commit -m "$comment"
+	git push
+fi
