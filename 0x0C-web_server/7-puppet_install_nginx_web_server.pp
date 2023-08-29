5733c69b2cda3bb17f3550dcd3731c6d564e0d1b
bossclement <bossclement@gmail.com>
2023-08-29 22:45:34 +0200

    New commit

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,58 +1,28 @@
 # install NGINX usping puppet
 
-class webserver {
-    package { 'nginx':
-        ensure => 'installed',
-    }
+# Script to install nginx using puppet
 
-    file { '/var/www/html/index.html':
-        content => 'Hello World!',
-    }
-
-    service { 'nginx':
-        ensure => 'running',
-        enable => true,
-    }
-
-    file { '/etc/nginx/sites-available/default':
-        ensure  => 'absent',
-        require => Package['nginx'],
-    }
-
-    file { '/etc/nginx/sites-available/redirect_me':
-        content => "
-server {
-    listen 80;
-    server_name _;
-
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-
-    location / {
-        root /var/www/html;
-        index index.html;
-    }
-}",
-        require => Package['nginx'],
-    }
-
-    file { '/etc/nginx/sites-enabled/redirect_me':
-        ensure  => 'link',
-        target  => '/etc/nginx/sites-available/redirect_me',
-        require => File['/etc/nginx/sites-available/redirect_me'],
-    }
-
-    file { '/etc/nginx/sites-enabled/default':
-        ensure  => 'absent',
-        require => Package['nginx'],
-    }
-
-    exec { 'nginx_reload':
-        command => 'service nginx reload',
-        require => [Package['nginx'], File['/etc/nginx/sites-enabled/redirect_me']],
-    }
+package {'nginx':
+  ensure => 'present',
 }
 
-class { 'webserver': }
+exec {'install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
+
+}
+
+exec {'Hello':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
+}
+
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/youtube.com/watch?v=QH2-TGUlwu4\/;\\n\\t}/" /etc/nginx/sites-available/default':
+  provider => shell,
+}
+
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
 
