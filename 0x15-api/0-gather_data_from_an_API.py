12b83bc8eebbe25e47b619de67ecfe65448780a9
bossclement <bossclement@gmail.com>
2023-09-25 17:45:15 +0200

    New commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
 # script that sends a get request to a REST API
+
+
 import requests
 import sys
 
 
 if __name__ == "__main__":
     employeeId = sys.argv[1]
-    baseUrl = "https://jsonplaceholder.typicode.com/users"
-    url = baseUrl + "/" + employeeId
+    URL = "https://jsonplaceholder.typicode.com/users"
+    link = URL + "/" + employeeId
 
-    response = requests.get(url)
+    response = requests.get(link)
     employeeName = response.json().get('name')
 
-    todoUrl = url + "/todos"
-    response = requests.get(todoUrl)
-    tasks = response.json()
+    tdlink = link + "/todos"
+    response = requests.get(tdlink)
+    todos = response.json()
     done = 0
     done_tasks = []
 
-    for task in tasks:
+    for task in todos:
         if task.get('completed'):
             done_tasks.append(task)
             done += 1
 
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employeeName, done, len(tasks)))
+    print("Employee {} is done with todos({}/{}):"
+          .format(employeeName, done, len(todos)))
 
     for task in done_tasks:
         print("\t {}".format(task.get('title')))
