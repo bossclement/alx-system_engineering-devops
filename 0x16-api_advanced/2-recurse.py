b3f3bb69817d38672064baafb60025180bc73ad9
bossclement <bosstclement@gmail.com>
2023-10-29 11:52:46 +0200

    New commit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,28 +3,30 @@
 2-recurse
 """
 import requests
-a = None
+after = None
 
 
 def recurse(subreddit, hot_list=[]):
     """
     queries the Reddit API and returns a list containing the
     titles of all hot articles for a given subreddit"""
-    global a
-    agent = {'User-Agent': 'api_advanced-project'}
+    global after
+    user_agent = {'User-Agent': 'api_advanced-project'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    parameters = {'after': a}
-    results = requests.get(url, params=parameters, headers=agent,
+    parameters = {'after': after}
+    results = requests.get(url, params=parameters, headers=user_agent,
                            allow_redirects=False)
 
     if results.status_code == 200:
-        a_d = results.json().get("data").get("after")
-        if a_d is not None:
-            a = a_d
+        after_data = results.json().get("data").get("after")
+        if after_data is not None:
+            after = after_data
             recurse(subreddit, hot_list)
-        ts = results.json().get("data").get("children")
-        for title_ in ts:
+        all_titles = results.json().get("data").get("children")
+        for title_ in all_titles:
             hot_list.append(title_.get("data").get("title"))
         return hot_list
     else:
         return (None)
+
+print(recurse("Hi"))
