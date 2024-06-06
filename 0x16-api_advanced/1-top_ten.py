d2bf8f40625796da1493bdb5b2e81cf75a7d75f4
bossclement <122975092+bossclement@users.noreply.github.com>
2024-06-07 00:21:34 +0200

    changes

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-1-top_ten
+top-ten
 """
 
 from requests import get
@@ -9,23 +9,23 @@ from requests import get
 
 def top_ten(subreddit):
     """
-    unction that queries the Reddit API and prints the titles of the
-    first 10 hot posts listed for a given subreddit.
+    queries Reddit API
     """
 
     if subreddit is None or not isinstance(subreddit, str):
         print("None")
 
-    agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     params = {'limit': 10}
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
-    res = get(url, headers=agent, params=params).json()
+    response = get(url, headers=user_agent, params=params)
+    results = response.json()
 
     try:
-        data = res.get('data').get('children')
+        my_data = results.get('data').get('children')
 
-        for i in data:
+        for i in my_data:
             print(i.get('data').get('title'))
 
     except Exception:
